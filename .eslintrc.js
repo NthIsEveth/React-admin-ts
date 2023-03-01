eb1888243e6944e220111581c75d89c852d33895
刘占硕 <liuzhanshuoc@163.com>
2023-03-01 17:43:03 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    eqeqeq: ['error', 'always'],
+    'prefer-destructuring': 2,
+    'no-duplicate-imports': 2,
+    'no-useless-rename': 2,
+    'no-array-constructor': 2,
+    'prefer-object-spread': 'error',
+    'max-len': ['error', { code: 150 }],
+    'quote-props': ['error', 'as-needed'],
+    'space-before-blocks': 'error',
+    'keyword-spacing': ['error', { before: true }],
+    'space-infix-ops': 'error',
+    'eol-last': ["error", "always"],
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+    'no-whitespace-before-property': 'error',
+  }
+};
